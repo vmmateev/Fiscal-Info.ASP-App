41db9cde878c21850952b086c48981a540655713
vmmateev <v.m.mateev@gmail.com>
2021-08-21 13:52:39 +0300

    Refactoring

--- src/Web/FiscalInfoApp.Web.ViewModels/FuelDispenser/2PetrolStationViewModelDropDown.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FiscalInfoApp.Web.ViewModels.FuelDispenser
-{
-    public class PetrolStationViewModelTESTDropDown
-    {
-        public int Id { get; set; }
-
-        public string Name { get; set; }
-    }
-}
